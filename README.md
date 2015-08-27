3563403b0378041726ff358f2e2d6bffb79386d3
Dan Nguyen <dansonguyen@gmail.com>
2015-08-26 19:14:56 -0700

    isotope magic

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # seeing-is-beliebing
-Instagram util for finding photos taken shorty before and after near where another photo was taken
+Instagram util for finding photos taken shorty before and after near where another photo was taken.
+
+
+My phone footage of fans [waiting for Justin Bieber to leave WHTZ's building in Tribeca](https://www.youtube.com/watch?v=-lNVGbobZFA).
+
+
+Given an Instagram image, this script uses Instagram's API to find all photos within 500 meters and uploaded within 30 minutes before and 240 minutes after of the supplied Instagram image. It then creates a filterable webpage of those images.
+
 
 
 Usage:
@@ -11,7 +18,22 @@ Assuming that your [Instagram access token](http://www.compciv.org/recipes/data/
     $ python3 beliebe.py "https://instagram.com/p/6xXvJqwi-k"
 ~~~
 
-Returns a list of photos within TK_DEFAULT feet and TK_BEFORE minutes and TK_AFTER minutes within the supplied Instagram image URL or ID.
 
 
-(Note: currently only returns data from the given photo)
+## TODO:
+
+- Set origin image
+- Add haversine
+- Add data to each image
+  - lat/lng
+  - distance 
+  - comments
+  - text as a span
+- Add sort buttons for:
+  + Likes
+  + Comments
+  + Time 
+  + Distance
+- Add filter buttons for:
+  + Before/after
+- Add filter text
--- template/js/app.js
+++ template/js/app.js
@@ -10,29 +10,60 @@ $(document).ready(function(){
 
 
 function decoratePage(images){
+  insertImages(images);
+  doIsotope();
+}
+
+function insertImages(images){
   console.info("whatup")
   var $body     = $("body"),
-      $content  = $('<div id="content" class="container">'),
-      $imagerow = $('<div id="images" class="row">');
-  $content.append($imagerow)
-  $body.append($content);
+      $content  = $('<div id="content" class="container"></div>'),
+      $imagerow = $('<div id="images"></div>');
   $content.append("<h1>Images</h1>")
   $content.append("<h5>" + images.length + " found</h5>")
+  $content.append($imagerow)
+  $body.append($content);
   console.log(images.length)
   // now add images
   for(var i = 0, lth = images.length; i < lth; i++){
-    console.log(i)
     var image = images[i];
     var page_url  = image.link,
-        img_url   = image.images.thumbnail.url;
+        img_url   = image.images.thumbnail.url,
+        likes_count = image.likes['count'];
 
-    var $col  = $('<div class="col-sm-4"></div>'),
-        $a    = $('<a href="' + page_url + '"></a>'),
+    var $idiv  = $('<div class="image" data-likes-count="' +
+                    likes_count + '"></div>'),
+        $a    = $('<a target="_instagram" href="' + page_url + '"></a>'),
         $img  = $('<img src="' + img_url +'">');
 
     $a.append($img);
-    $col.append($a);
-    $imagerow.append($col);
-    console.info(image['id'])
+    $idiv.append($a);
+    $imagerow.append($idiv);
   }
 }
+
+function doIsotope(){
+  var $grid_el = $("#images")
+  console.info("in doIsotope")
+  $grid_el.isotope({
+    itemSelector: '.image',
+    layoutMode: 'fitRows'
+  });
+}
+
+
+function filterIsotope(){
+    var number = $(this).data('likes-count');
+    return parseInt( number) > 100;
+}
+
+
+$(document).ready(function(){
+  $("#filter-button").click(function(){
+    var $grid_el = $("#images");
+    console.info("filter button clicked")
+    $grid_el.isotope({
+      filter: filterIsotope
+    });
+  })
+});
